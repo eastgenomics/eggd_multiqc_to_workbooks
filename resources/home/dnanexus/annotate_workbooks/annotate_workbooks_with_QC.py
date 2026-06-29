faf3c49fffcbf7e62aaaf70ecb3755ffaf05169b
Victoria Fuller <victoriaamaya1@yahoo.co.uk>
2026-06-29 11:09:13 +0100

    Linting

--- resources/home/dnanexus/annotate_workbooks/annotate_workbooks_with_QC.py
+++ resources/home/dnanexus/annotate_workbooks/annotate_workbooks_with_QC.py
@@ -8,9 +8,9 @@ from pathlib import Path
 # get paths from bash commandline arguments
 # improvement - use argparse
 parser = argparse.ArgumentParser(
-    description="Annotate sample workbooks with QC metrics extracted from MultiQC outputs.")
+    description="Annotate sample workbooks with QC metrics")
 parser.add_argument("--multiqc_folder",
-                     help="Name of the MultiQC folder under multiqc_inputs/")
+                    help="Name of the MultiQC folder under multiqc_inputs/")
 parser.add_argument("--reports_folder",
                     help="Name of the reports folder under reports_inputs/")
 parser.add_argument("--config_json",
@@ -25,7 +25,7 @@ config_json = args.config_json
 file_suffix = args.file_suffix
 
 # read config string into dict
-config_file=json.loads(config_json)
+config_file = json.loads(config_json)
 
 print(config_file)
 
@@ -35,6 +35,7 @@ reports_path = Path("reports_inputs") / reports_folder
 
 print(multiqc_path)
 print(reports_path)
+
 
 def annotate_workbook(sample_row, reports_path):
     """
@@ -54,8 +55,8 @@ def annotate_workbook(sample_row, reports_path):
             )
         coverage_string = f"{coverage}%"
 
-       # The freeMix score is % contamination predicted
-       # conversion to percentage is now calculated during multiqc
+        # The freeMix score is % contamination predicted
+        # conversion to percentage is now calculated during multiqc
         contamination = round(
             (sample_row[
                 "verifybamid-FREEMIX"
@@ -70,7 +71,7 @@ def annotate_workbook(sample_row, reports_path):
                 ]), 1
             )
 
-        fold80 = round(sample_row["FOLD_80_BASE_PENALTY"],1)
+        fold80 = round(sample_row["FOLD_80_BASE_PENALTY"], 1)
 
         insert_size = int(sample_row[
             "picard_insertsizemetrics-summed_median"
@@ -87,10 +88,10 @@ def annotate_workbook(sample_row, reports_path):
                 sex_check = sample_row["Match_Sexes"]
                 sex_check_string = f"Somalier used. Sex match: {sex_check}"
             except KeyError as err:
-                print(f"[WARN] Column {err} missing for sample {sample}; skipping row.")
+                print(f"[WARN] {err}: column missing for {sample}; skipping")
                 return
     except KeyError as err:
-        print(f"[WARN] Column {err} missing for sample {sample}; skipping row.")
+        print(f"[WARN] {err}: column missing for {sample}; skipping")
         return
 
     # get workbook corresponding to sample
@@ -106,20 +107,28 @@ def annotate_workbook(sample_row, reports_path):
 
     # add "Somalier" to cell where header is currently added
 
-    worksheet[config_file.get("cell_locations",{}).get("somalier_text")] = "Sex Check"
+    worksheet[config_file.get(
+        "cell_locations", {}).get("somalier_text")] = "Sex Check"
 
     # add data to sheet
     # want to pass cell locations in via a config for customisation
-    worksheet[config_file.get("cell_locations",{}).get("250_coverage")] = coverage_string
-    worksheet[config_file.get("cell_locations",{}).get("freemix")] = contamination_string
-    worksheet[config_file.get("cell_locations",{}).get("M_reads")] = total_reads_M
-    worksheet[config_file.get("cell_locations",{}).get("fold_80")] = fold80
-    worksheet[config_file.get("cell_locations",{}).get("insert_size")] = insert_size_string
-    worksheet[config_file.get("cell_locations",{}).get("somalier")] = sex_check_string
+    worksheet[config_file.get(
+        "cell_locations", {}).get("250_coverage")] = coverage_string
+    worksheet[config_file.get(
+        "cell_locations", {}).get("freemix")] = contamination_string
+    worksheet[config_file.get(
+        "cell_locations", {}).get("M_reads")] = total_reads_M
+    worksheet[config_file.get(
+        "cell_locations", {}).get("fold_80")] = fold80
+    worksheet[config_file.get(
+        "cell_locations", {}).get("insert_size")] = insert_size_string
+    worksheet[config_file.get(
+        "cell_locations", {}).get("somalier")] = sex_check_string
 
     # save file
-    new_path= sample + file_suffix
+    new_path = sample + file_suffix
     sample_workbook.save(new_path)
+
 
 def create_combined_qc(multiqc_path):
     """
@@ -127,19 +136,23 @@ def create_combined_qc(multiqc_path):
     """
     # get general statistics, picard hs metrics and somalier
 
-    general_stats_path = multiqc_path / config_file.get("multiqc_file_names",{}).get("general_stats_file")
-    hsmetrics_path = multiqc_path / config_file.get("multiqc_file_names",{}).get("hsmetrics_file")
-    sexcheck_path = multiqc_path / config_file.get("multiqc_file_names",{}).get("sexcheck_file")
-    somalier_path = multiqc_path / config_file.get("multiqc_file_names",{}).get("somalier_file")
+    general_stats_path = multiqc_path / config_file.get(
+        "multiqc_file_names", {}).get("general_stats_file")
+    hsmetrics_path = multiqc_path / config_file.get(
+        "multiqc_file_names", {}).get("hsmetrics_file")
+    sexcheck_path = multiqc_path / config_file.get(
+        "multiqc_file_names", {}).get("sexcheck_file")
+    somalier_path = multiqc_path / config_file.get(
+        "multiqc_file_names", {}).get("somalier_file")
 
     # try finding sex check data, if not produced try somalier
     try:
-        sexcheck  = pd.read_csv(sexcheck_path, sep="\t")
+        sexcheck = pd.read_csv(sexcheck_path, sep="\t")
     except FileNotFoundError as e:
         print("sexcheck not found, looking for somalier")
         try:
             # if sex check file does not exist, find somalier check
-            sexcheck  = pd.read_csv(somalier_path, sep="\t")
+            sexcheck = pd.read_csv(somalier_path, sep="\t")
         except FileNotFoundError as e:
             sys.exit(f"[ERROR] Required MultiQC file missing: {e.filename}")
     except pd.errors.ParserError as e:
@@ -159,11 +172,10 @@ def create_combined_qc(multiqc_path):
 
     return combined_qc
 
+
 print("Beginning python")
 qc_table = create_combined_qc(multiqc_path)
 print(qc_table)
-qc_table.apply(annotate_workbook, axis=1, reports_path = reports_path)
+qc_table.apply(annotate_workbook, axis=1, reports_path=reports_path)
 
 print("Reports annotated")
-
-
